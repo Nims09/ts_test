bcf00b4afdca9b8bba7d846d990849a69546cbc5
Nims09 <nmots@live.ca>
2015-07-27 20:14:27 -0700

    added some tests for private functions and argument error

--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -126,12 +126,98 @@ describe Simulator do
 
       include_examples "correct_state"
     end
-
-    # TODO : Add some tests of your own.
-    context "#6" do
-      
-    end
-
   end
 
+  context "#private" do
+    let(:initial_state) do
+      [
+       [:soft, :none, :none],
+       [:hard, :none, :soft],
+       [:none, :none, :hard],
+      ]
+    end    
+
+    describe "#in_array_range?" do
+      it "should return true for a value in range" do
+        expect( simulator.send(:in_array_range?, 0, 0) ).to eq true
+      end
+
+      it "should return true for a value on the edge of range" do
+        expect( simulator.send(:in_array_range?, 0, (initial_state.size-1)) ).to eq true
+      end
+
+      it "should return false for a value outside of range" do
+        expect( simulator.send(:in_array_range?, 0, (initial_state.size)) ).to eq false
+      end
+    end
+
+    describe "#update_opinion_for" do
+      it "should return 'none'" do
+        sim = Simulator.new([
+         [:soft, :none],
+         [:hard, :none],
+        ])
+        expect( sim.send(:update_opinion_for, 0, 0) ).to eq :none
+      end 
+
+      it "should return 'none'" do
+        sim = Simulator.new([
+         [:soft, :hard, :hard],
+         [:hard, :soft, :soft],
+         [:none, :none, :hard],
+        ])    
+        expect( sim.send(:update_opinion_for, 1, 1) ).to eq :none
+      end 
+
+      it "should return 'soft'" do
+        sim = Simulator.new([
+         [:none, :soft],
+         [:hard, :soft],
+        ])    
+        expect( sim.send(:update_opinion_for, 0, 0) ).to eq :soft
+      end   
+
+      it "should return 'hard'" do
+        sim = Simulator.new([
+         [:none, :soft],
+         [:hard, :hard],
+        ])    
+        expect( sim.send(:update_opinion_for, 0, 0) ).to eq :hard
+      end        
+
+      it "should return 'none' without changing" do
+        sim = Simulator.new([
+         [:none, :none],
+         [:hard, :hard],
+        ])    
+        expect( sim.send(:update_opinion_for, 0, 0) ).to eq :none
+      end   
+
+      it "should return 'soft' without changing" do
+        sim = Simulator.new([
+         [:soft, :hard],
+         [:hard, :none],
+        ])    
+        expect( sim.send(:update_opinion_for, 0, 0) ).to eq :soft
+      end         
+    end 
+  end 
+
+  context "ArgumentError" do
+    it "should raise an ArgumentError when given bad input" do 
+      expect { Simulator.new([
+       [:not_an_expected_value, :hard, :none],
+       [:hard, :none, :soft],
+       [:none, :soft, :hard],
+      ])}.to raise_error
+    end
+
+    it "should not raise ArgumentError with expected input" do 
+      expect { Simulator.new([
+       [:soft, :hard, :none],
+       [:hard, :none, :soft],
+       [:none, :soft, :hard],
+      ])}.not_to raise_error
+    end    
+  end
 end
